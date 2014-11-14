56f247081b565cb32d440a48c15f6c7b75e3d99d
yeahnoob <zgemlbox@gmail.com>
2014-11-14 19:36:56 +0800

    minor tweak.

--- julia/ast_tweak.jl
+++ julia/ast_tweak.jl
@@ -1,74 +1,63 @@
-ADD = +
+############################################################
+# Using Julia-v0.3.2,
+#   from julialang.org.
+############################################################
 
-SUB = -
+const ADD = +
 
-DIV = /
+const SUB = -
 
-MUL = *
+const DIV = /
+
+const MUL = *
 
 function SUM (n::Array{Any, 1})
-    s = 0
+    s = 0.
     for item in n
         s += item
     end
     return s
 end
 
-function evalAST ( var )
-    oper = var[1]
+const ast = {SUM,
+        {SUB,{ADD,{DIV,{MUL,10.,20.},30.},40.},50.},
+        {SUB,{ADD,{DIV,{MUL,20.,30.},40.},50.},60.},
+        {SUB,{ADD,{DIV,{MUL,30.,40.},50.},60.},70.},
+        {SUB,{ADD,{DIV,{MUL,40.,50.},60.},70.},80.}
+}
+
+evalAST ( var::Float64 ) = var
+function evalAST ( var::Array{Any,1} )
     evalargs = {}
     for item in var[2:end]
-        if (typeof(item) == Array{Any,1})
-            t = evalAST(item)
-            push!(evalargs, t)
-        else
-            push!(evalargs, item)
-        end
+        push!(evalargs, evalAST(item))
     end
-    return oper == SUM ? oper(evalargs) : oper(evalargs[1],evalargs[2])
+    return var[1] == SUM ? SUM(evalargs) : var[1](evalargs[1], evalargs[2])
 end
 
-iterations = 100_000
-function timeAST ( var )
+function timeAST ( var::Array{Any,1}; Iterations::Int64 = 100_000 )
     result = 0
-    for i in 1:iterations
+    for i in 1:Iterations
         result += evalAST(var)
     end
     return result
 end
 
-ast = {SUM,
-        {SUB,{ADD,{DIV,{MUL,10.,20.},30.},40.},50.},
-        {SUB,{ADD,{DIV,{MUL,20.,30.},40.},50.},60.},
-        {SUB,{ADD,{DIV,{MUL,30.,40.},50.},60.},70.},
-        {SUB,{ADD,{DIV,{MUL,40.,50.},60.},70.},80.}
-}
-@time result = timeAST(ast)
-print("The result number of AST is ", iround(result), "\n")
-ast = {SUM,
-        {SUB,{ADD,{DIV,{MUL,50.,40.},30.},20.},10.},
-        {SUB,{ADD,{DIV,{MUL,60.,50.},40.},30.},20.},
-        {SUB,{ADD,{DIV,{MUL,70.,60.},50.},40.},30.},
-        {SUB,{ADD,{DIV,{MUL,80.,70.},60.},50.},40.}
-}
-@time result = timeAST(ast)
-print("The result number of AST is ", iround(result), "\n")
-ast = {SUM,
-        {SUB,{ADD,{DIV,{MUL,10.,20.},30.},40.},50.},
-        {SUB,{ADD,{DIV,{MUL,20.,30.},40.},50.},60.},
-        {SUB,{ADD,{DIV,{MUL,30.,40.},50.},60.},70.},
-        {SUB,{ADD,{DIV,{MUL,40.,50.},60.},70.},80.}
-}
-@time result = timeAST(ast)
-print("The result number of AST is ", iround(result), "\n")
-ast = {SUM,
-        {SUB,{ADD,{DIV,{MUL,50.,40.},30.},20.},10.},
-        {SUB,{ADD,{DIV,{MUL,60.,50.},40.},30.},20.},
-        {SUB,{ADD,{DIV,{MUL,70.,60.},50.},40.},30.},
-        {SUB,{ADD,{DIV,{MUL,80.,70.},60.},50.},40.}
-}
-@time result = timeAST(ast)
-print("The result number of AST is ", iround(result), "\n")
+# Fisrt Run 'timeAST()' , waiting for functions compiled
+print("[NOT benchmark. Waiting for functions compiled.]\n    ")
+@time timeAST({SUM, {MUL, {DIV, 1., 1.}, 1.}, {SUB, 1., 1.}, {ADD, 1., 1.}})
+print("\n")
+# Benchmark @ this time
+print("[Benchmark at this time.]\n")
+@time timeAST(ast)
+@time timeAST(ast)
+@time timeAST(ast)
+print("\n")
+
+#= Tweaking in julia REPL
+using ProfileView
+ProfileView.view()
+=#
 
 None
 
